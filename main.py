2c97724947157fc8fbe9c234f92f9a7a847e6b14
itsaxron <s10247312@connect.np.edu.sg>
2022-08-04 02:33:36 +0800

    change variable

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import api, coh, overheads, profit_loss
 
 def main():
-    forex = api.api_function()
-    overheads_max = overheads.overhead_function(forex)
-    pnl_losses = profit_loss.profitloss_function(forex)
-    coh_losses = coh.coh_function(forex)
+    forex = api.getExchangeRate()
+    maxOverhead = overheads.getMaxOverhead(forex)
+    losses = profit_loss.getProfitLosses(forex)
+    cohLosses = coh.getcohLosses(forex)
 
     with open('summary_report.txt', 'w') as f:
         f.write(f'[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n')
-        f.write(f'[HIGHEST OVERHEAD] {overheads_max[0].upper()}: SGD{overheads_max[1]}\n')
+        f.write(f'[HIGHEST OVERHEAD] {maxOverhead[0].upper()}: SGD{maxOverhead[1]}\n')
 
-        if coh_losses == []: # this means there were no losses
+        if cohLosses == []: # this means there were no losses
             f.write(f'[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
         else:
-            for i in range(len(coh_losses)):
-                f.write(f'[CASH DEFICIT] DAY: {coh_losses[i][0]}, AMOUNT: SGD{coh_losses[i][1]}\n') 
+            for i in range(len(cohLosses)):
+                f.write(f'[CASH DEFICIT] DAY: {cohLosses[i][0]}, AMOUNT: {cohLosses[i][1]}\n')
 
-        if pnl_losses == []: # this means there were no losses
+        if losses == []: # this means there were no losses
             f.write(f'[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
         else:
-            for i in range(len(pnl_losses)):
-                f.write(f'[PROFIT DEFICIT] DAY: {pnl_losses[i][0]}, AMOUNT: SGD{pnl_losses[i][1]}\n')
+            for i in range(len(losses)):
+                f.write(f'[PROFIT DEFICIT] DAY: {losses[i][0]}, AMOUNT: {losses[i][1]}\n')
 
-    print("Output Successful")
 
 main()
