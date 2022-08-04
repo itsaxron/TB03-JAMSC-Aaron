fe70249bbf0e1574ced27ca632aa214a85b3c85d
itsaxron <s10247312@connect.np.edu.sg>
2022-08-04 20:35:05 +0800

    run code

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import api, coh, overheads, profit_loss
 
 def main():
     forex = api.api_function()
-    overheads = overheads.overhead_function(forex)
+    overheads_max = overheads.overhead_function(forex)
     losses = profit_loss.profitloss_function(forex)
-    coh = coh.coh_function(forex)
+    cohdeficit = coh.coh_function(forex)
 
     with open('summary_report.txt', 'w') as f:
         f.write(f'[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n')
-        f.write(f'[HIGHEST OVERHEAD] {overheads[0].upper()}: SGD{overheads[1]}\n')
+        f.write(f'[HIGHEST OVERHEAD] {overheads_max[0].upper()}: SGD{overheads_max[1]}\n')
 
         if coh == []: # this means there were no losses
             f.write(f'[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
         else:
-            for i in range(len(coh)):
-                f.write(f'[CASH DEFICIT] DAY: {coh[i][0]}, AMOUNT: {coh[i][1]}\n')
+            for i in range(len(cohdeficit)):
+                f.write(f'[CASH DEFICIT] DAY: {cohdeficit[i][0]}, AMOUNT: {cohdeficit[i][1]}\n')
 
         if losses == []: # this means there were no losses
             f.write(f'[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
